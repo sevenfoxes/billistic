bcfd7488d81d7b38ecc0771e8abcddd01bf6da05
Scott Nesham <scott.nesham@asynchrony.com>
2018-06-19 16:34:45 -0500

    add firebase_auth and auth_actions

---
+++ lib/actions/auth_actions.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class LogIn {}
+
+class LogInSuccessful {
+  final FirebaseUser user;
+
+  LogInSuccessful({@required this.user});
+
+  @override
+  String toString() {
+    return 'LogIn{user: $user}';
+  }
+}
+
+class LogInFail {
+  final dynamic error;
+
+  LogInFail(this.error);
+
+  @override
+  String toString() {
+    return 'LogIn{There was an error loggin in: $error}';
+  }
+}
